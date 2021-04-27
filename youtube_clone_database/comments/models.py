64f1a100ad0d07595dd813806fccb0aeae9a0e57
DemetriceW20 <demetrice_williams@hotmail.com>
2021-04-26 18:07:45 -0800

    Added comment_reply field in Comments model.

--- youtube_clone_database/comments/models.py
+++ youtube_clone_database/comments/models.py
@@ -4,5 +4,6 @@ from django.db import models
 class Comments(models.Model):
     video = models.ForeignKey('youtube_clone.Video', default=0, on_delete=models.CASCADE)
     comment = models.CharField(max_length=120)
+    comment_reply = models.ForeignKey('comments.Comments', max_length=120, default=None, blank=True, on_delete=models.CASCADE)
     like = models.BooleanField(default=None, null=True, blank=True)
     dislike = models.BooleanField( default=None, null=True, blank=True)
