77642aee5213e11d269f813a31ccdd3a92b1b39c
DemetriceW20 <demetrice_williams@hotmail.com>
2021-04-29 09:18:39 -0800

    Merging Database

--- youtube_clone_database/comments/models.py
+++ youtube_clone_database/comments/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Comment(models.Model):
     video = models.ForeignKey('youtube_clone.Video', default=None, on_delete=models.CASCADE)
-    comment_text = models.CharField(max_length=120)
+    comment_text = models.CharField(max_length=120, blank=True)
     original_comment = models.ForeignKey('comments.Comment', blank=True, null=True, on_delete=models.CASCADE)
     like = models.BooleanField(default=None, null=True, blank=True)
     dislike = models.BooleanField( default=None, null=True, blank=True)
--- youtube_clone_database/comments/serializers.py
+++ youtube_clone_database/comments/serializers.py
@@ -5,4 +5,4 @@ from .models import Comment
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
-        fields = ['video', 'comment_text', 'original_comment', 'like', 'dislike']
+        fields = ['id', 'video', 'comment_text', 'original_comment', 'like', 'dislike']
