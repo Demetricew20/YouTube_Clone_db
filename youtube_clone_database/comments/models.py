04679e0bfa603ff6c301d7aebaa9efb395a3eb0a
DemetriceW20 <demetrice_williams@hotmail.com>
2021-04-30 08:01:58 -0800

    Change boolean fields in comment model

--- youtube_clone_database/comments/models.py
+++ youtube_clone_database/comments/models.py
@@ -5,5 +5,5 @@ class Comment(models.Model):
     video = models.ForeignKey('youtube_clone.Video', on_delete=models.CASCADE)
     comment_text = models.CharField(max_length=120, blank=True)
     original_comment = models.ForeignKey('comments.Comment', blank=True, null=True, on_delete=models.CASCADE)
-    like = models.BooleanField(default=None, null=True, blank=True)
-    dislike = models.BooleanField( default=None, null=True, blank=True)
+    like = models.BooleanField(default=False)
+    dislike = models.BooleanField( default=False)
